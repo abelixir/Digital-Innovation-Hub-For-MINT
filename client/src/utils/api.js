888ce5903c042184d3fa35b7607980b62d624e00
Abiy <abiyzenagabriel@gmail.com>
2026-08-21 11:01:19 +0300

    change the api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://digital-innovation-hub-for-mint.onrender.com/api";
+const API_BASE = import.meta.env.VITE_API_URL || "https://digital-innovation-hub-for-mint.onrender.com/api";
 
 export async function apiRequest(endpoint, options = {}) {
   const token = localStorage.getItem("dih_token");
